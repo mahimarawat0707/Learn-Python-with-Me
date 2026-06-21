91cf663b524e8e41d44c71a0f137b24ac1516473
Mahima <mahimarawat0707@gmail.com>
2026-06-21 23:22:46 +0530

    pandas

---
+++ Library/Pandas/Employee/group.py
@@ -0,0 +1,34 @@
+import pandas as pd 
+employees = pd.DataFrame({ 
+  "Employee":[ 
+    "Aman", 
+    "Priya", 
+    "Rahul", 
+    "Sneha", 
+    "Ankit", 
+    "Riya", 
+    "Karan", 
+    "Meena"
+  ], 
+  "Department":[ 
+    "IT", 
+    "HR", 
+    "IT", 
+    "HR", 
+    "Sales", 
+    "Sales", 
+    "IT", 
+    "HR"
+  ], 
+  "Salary":[ 
+    50000, 
+    45000, 
+    60000, 
+    48000, 
+    55000, 
+    52000, 
+    65000, 
+    47000 
+  ] 
+})
+print(employees)
