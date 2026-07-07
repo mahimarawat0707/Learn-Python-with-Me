00fecb9de6de3de10dbf83cbdf05514f4800325a
Mahima <mahimarawat0707@gmail.com>
2026-07-07 15:02:34 +0530

    pandas

---
+++ Library/Pandas/Employee/date.py
@@ -0,0 +1,70 @@
+import pandas as pd
+
+employees = pd.DataFrame({
+  "Employees": [
+    "Aman",
+    "Priya",
+    "Rahul",
+    "Sneha",
+    "Ankit"
+  ],
+
+  "JoiningDate": [
+    "2021-05-12", 
+    "2020-11-25", 
+    "2022-03-18", 
+    "2023-01-08", 
+    "2019-07-15"
+  ],
+
+  "LoginTime": [ 
+    "2026-07-01 09:15:30", 
+    "2026-07-01 10:45:12", 
+    "2026-07-02 08:30:45", 
+    "2026-07-03 11:20:10", 
+    "2026-07-04 07:55:25" 
+  ]
+})
+
+print(employees)
+
+print(employees.dtypes)
+
+employees["JoiningDate"] = pd.to_datetime(
+  employees["JoiningDate"]
+)
+print(employees.dtypes)
+
+employees["LoginTime"] = pd.to_datetime(employees["LoginTime"])
+print(employees)
+
+employees["Hour"] = employees["LoginTime"].dt.hour
+print(employees)
+
+employees["Minute"] = employees["LoginTime"].dt.minute
+print(employees)
+
+employees["Second"] = employees["LoginTime"].dt.second
+print(employees)
+
+employees["DayName"] = employees["LoginTime"].dt.day_name()
+print(employees)
+
+employees["MonthName"] = employees["LoginTime"].dt.month_name()
+print(employees)
+
+employees["Weekday"] = employees["LoginTime"].dt.weekday
+print(employees)
+
+employees["Start"] = employees["LoginTime"].dt.is_month_start
+print(employees)
+
+employees["End"] = employees["LoginTime"].dt.is_month_end
+print(employees)
+
+dates = pd.to_datetime([
+  "2024-06-01",
+  "2025-06-01"
+])
+
+dates.is_leap_year
